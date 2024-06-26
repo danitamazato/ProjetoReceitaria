6cf33e6125c5e81a914cab5680f4afba501a4baf
danitamazato <danielle.tamazato@gmail.com>
2024-06-26 20:37:04 -0300

    update

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,56 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") // aqui estou iniciando o express
+const router = express.Router() // aqui estou configurando a primeira parte da rota
+const { v4: uuidv4 } = require('uuid')
 
-const app = express()
-const porta = 3333
+const app = express() // aqui estou iniciando o app
+const porta = 3333 // aqui estou criando a porta
 
+// aqui estou criando lista inicial de mulheres
 const mulheres = [
     {
+        id: '1',
         nome: '1',
         imagem: '1',
         minibio: '1'
     },
     {
+        id: '2',
         nome: '2',
         imagem: '2',
         minibio: '2'
     },
     {
+        id: '3',
         nome: '3',
         imagem: '3',
         minibio: '3'
     }
 ]
 
+// GET
 function mostraMulheres(request, response) {
     response.json(mulheres)
 }
 
+// POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.boby.minibio
+    }
+
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+
+// PORTA
 function mostraPorta() {
     console.log('Servidor criado e rodando na porta', porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+app.use(router.get('/mulheres', mostraMulheres)) // segunda configuração da rota, onde configurei a tora GET/mulheres
+app.use(router.post('/mulheres', criaMulher)) // configurei rota POST/MULHERES
+app.listen(porta, mostraPorta) // servidor ouvindo a porta
